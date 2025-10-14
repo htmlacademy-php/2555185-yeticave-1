2da037d3c69649cfb99c148fafd2f1c90a7dd022
Alexmasya97 <sasa_97@mail.ru>
2025-10-15 03:15:38 +1000

    some shit

---
+++ lot.php
@@ -0,0 +1,52 @@
+<?php
+die(5);
+require_once('./helpers.php');
+require_once('./functions.php');
+require_once 'init.php';
+
+// Получаем категории
+
+$sql_categories = 'SELECT title, symbol_code FROM categories';
+$result_categories = mysqli_query($link, $sql_categories);
+if ($result_categories) {
+    $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
+}
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+$sql = 'SELECT l.id, l.title AS lots_title, l.description, l.image, l.start_price, l.bidding_step, l.created_at, l.end_date, c.title AS category_title '
+    . 'FROM lots l '
+    . 'JOIN categories c ON l.category_id = c.id '
+    . 'WHERE l.id = ?;';
+
+$stmt = db_get_prepare_stmt($link, $sql, [$id]);
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+$lot = mysqli_fetch_assoc($res);
+
+if(!$lot){
+
+    http_response_code(404);
+    $pageContent = include_template('404.php');
+
+    $layout = include_template('layout.php', [
+        'pageContent' => $pageContent,
+        'title' => 'Страница не найдена',
+        'categories' => $categories
+    ]);
+    print $layout;
+    exit;
+}
+
+$pageContent = include_template('lot-template.php', [
+    'lot' => $lot
+]);
+
+$layout = include_template('layout.php', [
+    'pageContent' => $pageContent,
+    'title' => $lot['lots_title'],
+    'categories' => $categories
+]);
+
+print $layout;
+?>
